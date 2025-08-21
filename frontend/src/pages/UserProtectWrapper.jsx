dbdfbc100bd73f565396457fa74ac8cfcbed7972
Harman1105 <harman.meet.2005@gmail.com>
2025-08-22 01:01:32 +0530

    Added User Protection for routing

---
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from 'react'
+import { UserDataContext } from '../context/UserContext'
+import { useNavigate } from 'react-router-dom'
+
+
+const UserProtectWrapper = ({children}) => {
+
+    const token = localStorage.getItem('token')
+    const navigate = useNavigate();
+
+    useEffect(() => {
+    if (!token) {
+      navigate('/login')
+    }   
+  }, [token, navigate])
+
+  return (
+    <div>
+      {children}
+    </div> 
+  )
+}
+
+export default UserProtectWrapper
