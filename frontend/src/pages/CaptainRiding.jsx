e87eb5aac72be4da3414e361814dfd93ce7fc548
Harman1105 <harman.meet.2005@gmail.com>
2025-08-28 21:24:03 +0530

    Added Captain Riding

---
+++ frontend/src/pages/CaptainRiding.jsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import logo from "../assets/logo.png"
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import FinishRide from '../components/FinishRide'
+
+const CaptainRiding = () => {
+
+    const [finishRidePanel,setFinishRidePanel] = useState(false)
+    const finishRidePanelRef = useRef(null)
+    
+    useGSAP(function(){
+        if(finishRidePanel){
+          gsap.to(finishRidePanelRef.current,{
+           transform : 'translateY(0)'
+         })
+        }else{
+         gsap.to(finishRidePanelRef.current,{
+           transform : 'translateY(100%)'
+         })
+        }
+       },[finishRidePanel])
+
+
+  return (
+    <div className='h-screen relative'>
+        
+    <div className='fixed p-3 top-0 flex justify-between w-screen'>
+      <img className='w-40 h-14' src={logo}></img>
+       <Link to='/home' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+        <i className=" text-lg font-medium ri-logout-box-r-line"></i>
+    </Link >
+    </div>
+    <div className='h-4/5'>
+    <img className='h-full w-full object-cover' src='https://miro.medium.com/v2/resize:fit:1100/format:webp/0*gwMx05pqII5hbfmX.gif' alt=''></img>            
+    </div>
+    <div 
+    className='h-1/5 p-4 bg-yellow-400 relative flex items-center justify-between'
+    onClick={()=>{
+        setFinishRidePanel(true)
+    }}>
+    <h5 
+       className='p-1 w-[95%] text-center absolute top-0'
+       onClick={()=>{
+        
+       }}><i className="text-3xl text-gray-200 ri-arrow-up-wide-line"></i></h5>
+        <h4 className='text-xl font-semibold'>4 km away</h4>
+        <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>
+            Complete Ride</button>
+                      
+    </div>
+
+    <div ref={finishRidePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10'>
+          <FinishRide setFinishRidePanel={setFinishRidePanel}/>
+      </div>
+    
+</div>
+  )
+}
+
+export default CaptainRiding
